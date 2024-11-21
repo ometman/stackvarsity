71ca15a1e638f88cde3ecb3b1334ededbba8fea7
ometman <revomet@gmail.com>
2024-11-21 03:04:09 -0600

    Update server.js, .env; create app.js, src dir and contrllers, middleware, models in src; move utils to src, db.js to utils

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,7 @@
-import { express } from 'express';
-const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const pool = require('./db');
-
-require('dotenv').config();
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.use('/api/auth', authRoutes);
+const app = require('./app');
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
