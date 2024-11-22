d0e4bb1f74589a03f9394688dd534aa3a997124f
ometman <revomet@gmail.com>
2024-11-22 15:37:53 -0600

    Modified src/utils/db.js for teting

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -8,8 +8,13 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 5432,
 });
+
+pool.connect()
+  .then(() => console.log('PostgreSQL connected successfully'))
+  .catch((err) => console.error('Error connecting to PostgreSQL', err));
+
 
 module.exports = pool;
 
