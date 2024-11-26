70878aba6c8c741d68bede680d87e77c732611c3
ometman <revomet@gmail.com>
2024-11-26 09:38:41 -0600

    Frontend directory restructure and connection update

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -3,7 +3,7 @@ const { registerUser, loginUser } = require('../controllers/userController');
 
 const router = express.Router();
 
-router.post('/register', registerUser);
+router.post('/signup', registerUser);
 router.post('/login', loginUser);
 
 module.exports = router;
