8659fe9b31f917a5e60c9e727c26c71c75677f20
ometman <revomet@gmail.com>
2025-01-02 21:08:45 -0600

    Modify userRoutes.js api function import

--- svbackend/routes/userRoutes.js
+++ svbackend/routes/userRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const {
   registerUser,
   loginUser,
-} = require('../controllers/authController');
+} = require('../api/auth/userAuth');
 
 const router = express.Router();
 
