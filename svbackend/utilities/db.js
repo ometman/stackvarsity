ec7645a5e8b700ef1a08e49749ab3a5e191af286
ometman <revomet@gmail.com>
2025-01-07 00:02:30 -0600

    Modify db.js

--- svbackend/utilities/db.js
+++ svbackend/utilities/db.js
@@ -1,26 +1,25 @@
-// const { Pool } = require('pg');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// const pool = new Pool({
-//   user: process.env.DB_USER,
-//   host: process.env.DB_HOST,
-//   database: process.env.DB_NAME,
-//   password: process.env.DB_PASSWORD,
-//   port: process.env.DB_PORT || 5432,
-// });
-
-// pool.connect()
-//   .then(() => console.log('PostgreSQL connected successfully'))
-//   .catch((err) => console.error('Error connecting to PostgreSQL', err));
-
-
-// module.exports = pool;
-
-
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
 const { Sequelize } = require('sequelize');
-const config = require('../config/config.json').development;
+const hstore = require('pg-hstore')();
+// const config = require('../config/config.json').development;
+
+
+dotenv.config();
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT || 5432,
+});
+
+// Connect to PostgreSQL and log or catch eror
+pool.connect()
+  .then(() => console.log('PostgreSQL connected successfully'))
+  .catch((err) => console.error('Error connecting to PostgreSQL', err));
+
 
 // Initialize Sequelize instance with PostgreSQL
 const sequelize = new Sequelize(config.database, config.user, config.password, {
@@ -29,4 +28,4 @@ const sequelize = new Sequelize(config.database, config.user, config.password, {
     logging: false
 });
 
-module.exports = sequelize;
+module.exports = sequelize, pool;
