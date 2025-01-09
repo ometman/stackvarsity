2c695696c7d0f4b9bdd7039ab75aa287c8ed68fe
ometman <revomet@gmail.com>
2025-01-09 14:28:21 -0600

    Add userModel.js in models

---
+++ svbackend/models/userModel.js
@@ -0,0 +1,42 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../db');
+
+const User = sequelize.define('User', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            len: [4, 20]  // Username length between 4 and 20 characters
+        }
+    },
+    first_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    last_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+        validate: {
+            isEmail: true  // Validates proper email format
+        }
+    },
+    password_hash: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, { 
+    timestamps: true 
+});
+
+module.exports = User;
