e0c0cb43d3cda3b316a2e6468ec112fbb4fb7b92
ometman <revomet@gmail.com>
2025-01-09 16:20:15 -0600

    Update userModel.js in models

--- svbackend/models/userModel.js
+++ svbackend/models/userModel.js
@@ -13,36 +13,55 @@ const User = sequelize.define('User', {
         allowNull: false,
         unique: true,
         validate: {
-            len: [4, 20]  // Username length between 4 and 20 characters
+            len: [6, 20],  // Username length between 4 and 20 characters
+            isAlphanumeric: true // Only letters and numbers allowed  
         }
     },
     first_name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: [2, 40]
+        }
     },
     last_name: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate: {
+            len: [2, 40]
+        }
     },
     email: {
         type: DataTypes.STRING,
         unique: true,
         allowNull: false,
         validate: {
-            isEmail: true  // Validates proper email format
+            isEmail: true // Validates proper email format
         }
     },
     password_hash: {
         type: DataTypes.STRING,
         allowNull: false
     },
+    password_salt: {
+        type: DataTypes.STRING, 
+        allowNull: false 
+    }, 
     role: {
         type: DataTypes.ENUM('student', 'admin'), // Enum for fixed roles
         allowNull: false,
         defaultValue: 'student'
-    }
+    },
+    is_active: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: true // New users are active by default
+    },
 }, { 
-    timestamps: true 
+    timestamps: true,
+    indexes: [
+        { unique: true, fields: ['email'] }
+    ] 
 });
 
 module.exports = User;
