8beb7996f5a1ca2aab50da0161f6abdb763793b4
ometman <revomet@gmail.com>
2025-01-14 02:00:16 +0000

    Add answer model

---
+++ svbackend/models/Answer.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utilities/dbSequel');
+const Question = require('./Question');
+
+const Answer = sequelize.define('Answer', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    answer_text: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    is_correct: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+    }
+}, { timestamps: true, paranoid: true });
+
+Answer.belongsTo(Question, { foreignKey: 'question_id', onDelete: 'CASCADE' });
+
+module.exports = Answer;
