b71ea35ccfeab62f65df01aa86456def9c9748d9
ometman <revomet@gmail.com>
2025-01-14 02:00:51 +0000

    Add certificate model

---
+++ svbackend/models/Certificate.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utilities/dbSequel');
+const Student = require('./Student');
+const Course = require('./Course');
+
+const Certificate = sequelize.define('Certificate', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    issue_date: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW
+    },
+    certificate_url: {
+        type: DataTypes.STRING,
+        validate: { isUrl: true }
+    }
+}, { timestamps: true, paranoid: true });
+
+Certificate.belongsTo(Student, { foreignKey: 'student_id', onDelete: 'CASCADE' });
+Certificate.belongsTo(Course, { foreignKey: 'course_id', onDelete: 'CASCADE' });
+
+module.exports = Certificate;
