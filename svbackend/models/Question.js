c3f74436b90b12b71e28eeb91e3b8617a5d3b233
ometman <revomet@gmail.com>
2025-01-14 01:59:08 +0000

    Add Question model

---
+++ svbackend/models/Question.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../utilities/dbSequel');
+const Quiz = require('./Quiz');
+
+const Question = sequelize.define('Question', {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+    },
+    question_text: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    question_type: {
+        type: DataTypes.ENUM('multiple_choice', 'true_false', 'short_answer'),
+        allowNull: false
+    }
+}, { timestamps: true, paranoid: true });
+
+Question.belongsTo(Quiz, { foreignKey: 'quiz_id', onDelete: 'CASCADE' });
+
+module.exports = Question;
