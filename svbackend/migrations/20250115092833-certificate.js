7ec397e9c50ef6890191860a3ba01d4fc3cc6e7e
ometman <revomet@gmail.com>
2025-01-15 10:21:25 +0000

    Add certification migration

--- svbackend/migrations/20250115092833-certificate.js
+++ svbackend/migrations/20250115092833-certificate.js
@@ -1,22 +1,50 @@
 'use strict';
 
 /** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+module.exports = {
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('answers', {
+            id: {
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDV4,
+                allowNull: false,
+                primaryKey: true
+            },
+            answer_text: {
+                type: Sequelize.STRING,
+                allowNull: false
+            },
+            is_correct: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: false
+            },
+            question_id: {
+                type: Sequelize.UUID,
+                allowNull: false,
+                references: {
+                    model: 'questions', // This must match the table name of the Question model
+                    key: 'id'
+                },
+                onDelete: 'CASCADE'
+            },
+            created_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW
+            },
+            updated_at: {
+                type: Sequelize.DATE,
+                allowNull: false,
+                defaultValue: Sequelize.NOW
+            },
+            deleted_at: {
+                type: Sequelize.DATE
+            }
+        });
+    },
+
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('answers');
+    }
 };
