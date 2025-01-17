cb187fe8b276b38d9e3319d7773a0e3fb9bfbbf2
ometman <revomet@gmail.com>
2025-01-17 00:44:11 +0000

    Fix seeding for students

--- svbackend/seeders/seedStudents.js
+++ svbackend/seeders/seedStudents.js
@@ -1,49 +1,86 @@
 const { v4: uuidv4 } = require('uuid');
-const bcrypt = require('bcryptjs');
 
-const students = [
-  {
-    id: uuidv4(),
-    user_id: uuidv4(),
-    first_name: 'John',
-    last_name: 'Doe',
-    bio: 'A passionate learner.',
-    date_of_birth: new Date('2000-05-15'),
-    country: 'Ghana',
-    registration_date: new Date(),
-    last_login: new Date(),
-    profile_picture: 'https://example.com/john.jpg',
-    level: 'Beginner',
-    is_active: true
+const generateStudents = async (users) => {
+  return [
+    {
+      id: uuidv4(),
+      user_id: users.find(
+        user => user.role === 'student' && user.email === 'student1@example.com'
+        ).id || null,
+      first_name: 'Student',
+      last_name: 'One',
+      bio: 'A passionate learner.',
+      date_of_birth: new Date('2000-05-15'),
+      country: 'Ghana',
+      registration_date: new Date(),
+      last_login: new Date(),
+      profile_picture: 'https://example.com/john.jpg',
+      level: 'Beginner',
+      is_active: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+      deleted_at: new Date()
+    },
+    {
+      id: uuidv4(),
+      user_id: users.find(
+        user => user.role === 'student' && user.email === 'student2@example.com'
+        ).id || null,
+      first_name: 'Student',
+      last_name: 'Two',
+      bio: 'Aspiring software developer.',
+      date_of_birth: new Date('1998-11-20'),
+      country: 'Nigeria',
+      registration_date: new Date(),
+      last_login: new Date(),
+      profile_picture: 'https://example.com/jane.jpg',
+      level: 'Intermediate',
+      is_active: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+      deleted_at: new Date()
+    },
+    {
+      id: uuidv4(),
+      user_id: users.find(
+        user => user.role === 'student' && user.email === 'student3@example.com'
+        ).id || null,
+      first_name: 'Student',
+      last_name: 'Three',
+      bio: 'Web developer and design enthusiast.',
+      date_of_birth: new Date('1995-02-10'),
+      country: 'Kenya',
+      registration_date: new Date(),
+      last_login: new Date(),
+      profile_picture: 'https://example.com/paul.jpg',
+      level: 'Advanced',
+      is_active: true,
+      created_at: new Date(),
+      updated_at: new Date(),
+      deleted_at: new Date()
+    }
+  ];
+};
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+      const users = await queryInterface.sequelize.query(
+          `SELECT id, role, email FROM "users" 
+          WHERE 
+            role = 'student' 
+            AND email IN (
+              'student1@example.com',
+              'student2@example.com',
+              'student3@example.com'
+          );`,
+          { type: Sequelize.QueryTypes.SELECT }
+      );
+
+      const Students = await generateStudents(users);
+      await queryInterface.bulkInsert('students', Students);
   },
-  {
-    id: uuidv4(),
-    user_id: uuidv4(),
-    first_name: 'Jane',
-    last_name: 'Smith',
-    bio: 'Aspiring software developer.',
-    date_of_birth: new Date('1998-11-20'),
-    country: 'Nigeria',
-    registration_date: new Date(),
-    last_login: new Date(),
-    profile_picture: 'https://example.com/jane.jpg',
-    level: 'Intermediate',
-    is_active: true
-  },
-  {
-    id: uuidv4(),
-    user_id: uuidv4(),
-    first_name: 'Paul',
-    last_name: 'Adams',
-    bio: 'Web developer and design enthusiast.',
-    date_of_birth: new Date('1995-02-10'),
-    country: 'Kenya',
-    registration_date: new Date(),
-    last_login: new Date(),
-    profile_picture: 'https://example.com/paul.jpg',
-    level: 'Advanced',
-    is_active: true
+
+  down: async (queryInterface, Sequelize) => {
+      await queryInterface.bulkDelete('students', null, {});
   }
-];
-
-module.exports = students;
+};
