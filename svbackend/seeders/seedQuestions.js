f7366064cbf811578733e59950460f1d70405cb6
ometman <revomet@gmail.com>
2025-02-03 00:43:27 +0000

    Add seed data to questions table

--- svbackend/seeders/seedQuestions.js
+++ svbackend/seeders/seedQuestions.js
@@ -1,94 +1,96 @@
 'use strict';
 
+const { v4: uuidv4 } = require('uuid');
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkInsert('Questions', [
+    await queryInterface.bulkInsert('questions', [
       {
-        id: 'question_uuid1',
+        id: uuidv4(),
         question_text: 'What does HTML stand for?',
         question_type: 'multiple_choice',
-        quiz_id: 'quiz_uuid1',
+        quiz_id: 'ac0bd5fa-1482-41d1-8b7a-5f8eddbeecd4',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid2',
-        question_text: 'Explain the purpose of CSS Flexbox.',
-        question_type: 'open_ended',
-        quiz_id: 'quiz_uuid1',
+        id: uuidv4(),
+        question_text: 'State the purpose of CSS Flexbox.',
+        question_type: 'short_answer',
+        quiz_id: 'ac0bd5fa-1482-41d1-8b7a-5f8eddbeecd4',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid3',
+        id: uuidv4(),
         question_text: 'What is the difference between var, let, and const?',
         question_type: 'multiple_choice',
-        quiz_id: 'quiz_uuid1',
+        quiz_id: 'ac0bd5fa-1482-41d1-8b7a-5f8eddbeecd4',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid4',
+        id: uuidv4(),
         question_text: 'What is a React component?',
         question_type: 'multiple_choice',
-        quiz_id: 'quiz_uuid2',
+        quiz_id: 'dd4fd942-0760-403c-919a-7383c0d0a8ed',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid5',
+        id: uuidv4(),
         question_text: 'Explain the concept of props in React.',
-        question_type: 'open_ended',
-        quiz_id: 'quiz_uuid2',
+        question_type: 'short_answer',
+        quiz_id: 'dd4fd942-0760-403c-919a-7383c0d0a8ed',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid6',
+        id: uuidv4(),
         question_text: 'What is the use of React Hooks?',
         question_type: 'multiple_choice',
-        quiz_id: 'quiz_uuid2',
+        quiz_id: 'dd4fd942-0760-403c-919a-7383c0d0a8ed',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid7',
+        id: uuidv4(),
         question_text: 'Explain the concept of RESTful APIs.',
-        question_type: 'open_ended',
-        quiz_id: 'quiz_uuid3',
+        question_type: 'short_answer',
+        quiz_id: 'b49b2433-62f2-483e-82e9-8096d3395179',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid8',
+        id: uuidv4(),
         question_text: 'What is middleware in Express.js?',
         question_type: 'multiple_choice',
-        quiz_id: 'quiz_uuid3',
+        quiz_id: 'b49b2433-62f2-483e-82e9-8096d3395179',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid9',
+        id: uuidv4(),
         question_text: 'How does PostgreSQL handle relational data?',
-        question_type: 'open_ended',
-        quiz_id: 'quiz_uuid3',
+        question_type: 'short_answer',
+        quiz_id: 'b49b2433-62f2-483e-82e9-8096d3395179',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
       },
       {
-        id: 'question_uuid10',
+        id: uuidv4(),
         question_text: 'Explain the importance of indexing in databases.',
         question_type: 'multiple_choice',
-        quiz_id: 'quiz_uuid3',
+        quiz_id: 'b49b2433-62f2-483e-82e9-8096d3395179',
         created_at: new Date(),
         updated_at: new Date(),
         deleted_at: null,
@@ -97,6 +99,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Questions', null, {});
+    await queryInterface.bulkDelete('questions', null, {});
   }
 };
--- svbackend/seeders/seedQuizzes.js
+++ svbackend/seeders/seedQuizzes.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
